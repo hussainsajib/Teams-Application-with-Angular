b0a416a359d080201d6a52f89b6fc4a358213487
hussain <m.hussainul.islam@gmail.com>
2019-07-15 11:54:44 -0400

    addition: skeleton of HomeComponent, EmployeesComponent, PositionsComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { PositionsComponent } from './positions/positions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     ContentComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    EmployeesComponent,
+    PositionsComponent
   ],
   imports: [
     BrowserModule
