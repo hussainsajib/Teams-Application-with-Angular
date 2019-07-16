3f88f848c7c26b519532a4e8c190f1690565123d
hussain <m.hussainul.islam@gmail.com>
2019-07-16 08:42:26 -0400

    working on employeesComponent

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Employee } from '../data/employee';
+import { EmployeeService } from '../data/employee.service'
+
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
   styleUrls: ['./employees.component.css']
 })
 export class EmployeesComponent implements OnInit {
+  employees: Employee[];
+  getEmployeesSub: any;
+  loadingError = false;
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService) { }
 
   ngOnInit() {
+    this.employees = this.employeeService<Employee[]>.getEmployee();
   }
 
 }
