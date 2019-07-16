1da193043d5047c33ed832adced1de88a62eac8c
hussain <m.hussainul.islam@gmail.com>
2019-07-15 21:35:29 -0400

    Part 2 - step 2 done

--- src/app/data/employee.service.ts
+++ src/app/data/employee.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Employee } from './employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  employeeUrl = 'https://glacial-beyond-73904.herokuapp.com/positions'
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getEmployee(): Observable<Employee []>{
+    return this.http.get<Employee[]>(this.employeeUrl);
+  }
 }
